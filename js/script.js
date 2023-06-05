c5cd07d92811950eb6e864581a85010bb9518175
sashaspasojevic <spsjvcsasa@yahoo.com>
2023-06-05 18:37:25 +0200

    add traveling section for desktop

--- js/script.js
+++ js/script.js
@@ -56,3 +56,27 @@ var swiper = new Swiper(".mySwiper", {
   nextButton: "#js-prev1",
   prevButton: "#js-next1",
 });
+
+// accordion
+
+const acc_buttons = document.querySelectorAll(".accordion__header");
+const acc_contents = document.querySelectorAll(".accordion__body");
+
+acc_buttons.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    acc_contents.forEach((acc) => {
+      if (
+        e.target.nextElementSibling !== acc &&
+        acc.classList.contains("active")
+      ) {
+        acc.classList.remove("active");
+        acc_buttons.forEach((btn) => {
+          btn.classList.remove("active");
+        });
+      }
+    });
+    const panel = btn.nextElementSibling;
+    panel.classList.toggle("active");
+    btn.classList.toggle("active");
+  });
+});
